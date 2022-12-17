3c9ffab36e37903d595680f1c0d7d6cf22c9ad72
Lucas Liégeois <lucasl@live.be>
2022-12-17 19:58:11 +0100

    day 1

---
+++ src/solutions/day_1.py
@@ -0,0 +1,31 @@
+from src.input_util import get_input
+
+
+def part1(input_data: str):
+    best = current = 0
+    for line in input_data.split('\n'):
+        if line:
+            current += int(line)
+        else:
+            best = max(current, best)
+            current = 0
+
+    return best
+
+
+def part2(input_data: str):
+    totals = []
+    current = 0
+    for line in input_data.split('\n'):
+        if line:
+            current += int(line)
+        else:
+            totals.append(current)
+            current = 0
+
+    return sum(sorted(totals)[-3:])
+
+
+if __name__ == '__main__':
+    print(f'Solution for day 1 is: {part1(get_input())}')
+    print(f'Solution for day 2 is: {part2(get_input())}')
---
+++ src/template.py
@@ -0,0 +1,14 @@
+from src.input_util import get_input
+
+
+def part1(input_data: str):
+    pass
+
+
+def part2(input_data: str):
+    pass
+
+
+if __name__ == '__main__':
+    print(f'Solution for day 1 is: {part1(get_input())}')
+    print(f'Solution for day 2 is: {part2(get_input())}')
