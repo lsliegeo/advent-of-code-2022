5392971fb7dee83524606f7fc8dd6ba571406ba1
Lucas Liégeois <lucasl@live.be>
2022-12-18 14:21:15 +0100

    add assertion to template

--- src/input_util.py
+++ src/input_util.py
@@ -5,6 +5,9 @@ from pathlib import Path
 def get_input() -> str:
     """Parse the day from the script name, open the corresponding input file."""
     script_file_name = Path(sys.argv[0]).name
-    day = int(''.join(c for c in script_file_name if c.isdigit()))
+    try:
+        day = int(''.join(c for c in script_file_name if c.isdigit()))
+    except ValueError:
+        raise ValueError(f'Unexpected script name: {script_file_name}')
     input_file = Path(__file__).parent.parent / 'input' / f'{day}.txt'
     return input_file.read_text()
--- src/template.py
+++ src/template.py
@@ -1,5 +1,11 @@
 from src.input_util import get_input
 
+EXAMPLE_PART_1 = """
+"""
+
+EXAMPLE_PART_2 = """
+"""
+
 
 def part1(input_data: str):
     pass
@@ -10,5 +16,7 @@ def part2(input_data: str):
 
 
 if __name__ == '__main__':
+    assert part1(EXAMPLE_PART_1) == None
     print(f'Solution for part 1 is: {part1(get_input())}')
+    assert part2(EXAMPLE_PART_2) == None
     print(f'Solution for part 2 is: {part2(get_input())}')
