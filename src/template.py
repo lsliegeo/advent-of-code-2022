2f08f90e345f92c4f993018b1d87c8a0b513f410
Lucas Liégeois <lucasl@live.be>
2022-12-24 15:50:15 +0100

    update template to single example

--- src/template.py
+++ src/template.py
@@ -1,8 +1,6 @@
 from src.input_util import get_input
 
-EXAMPLE_PART_1 = """"""
-
-EXAMPLE_PART_2 = """"""
+EXAMPLE = """"""
 
 
 def part1(input_data: str):
@@ -14,7 +12,7 @@ def part2(input_data: str):
 
 
 if __name__ == '__main__':
-    assert part1(EXAMPLE_PART_1) == None
+    assert part1(EXAMPLE) == None
     print(f'Solution for part 1 is: {part1(get_input())}')
-    assert part2(EXAMPLE_PART_2) == None
+    assert part2(EXAMPLE) == None
     print(f'Solution for part 2 is: {part2(get_input())}')
