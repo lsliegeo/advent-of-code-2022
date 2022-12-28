6cdabae3acc0bbfef6916053c13929a8480f1da6
Lucas Liégeois <lucasl@live.be>
2022-12-28 16:32:05 +0100

    ContextTimer threshold

--- src/timer_util.py
+++ src/timer_util.py
@@ -3,8 +3,11 @@ import timeit
 
 class ContextTimer:
 
-    def __enter__(self):
+    def __enter__(self, threshold: float = 3):
+        self.threshold = threshold
         self.start = timeit.default_timer()
 
     def __exit__(self, *args, **kwargs):
-        print(f'duration: {timeit.default_timer() - self.start:.02f}s')
+        duration = timeit.default_timer() - self.start
+        if duration > self.threshold:
+            print(f'duration: {duration:.02f}s')
